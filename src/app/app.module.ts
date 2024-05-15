af002b5b804751b7fb0b801350959d4eb30d9fcc
Kushan Yasiru <kushanyasiruwalpola@gmail.com>
2024-05-15 14:42:34 +0530

    Create Angular UI

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DepartmentsComponent } from './pages/departments/departments.component';
+import { EmployeeRegistrationComponent } from './pages/employee-registration/employee-registration.component';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DepartmentsComponent,
+    EmployeeRegistrationComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/pages/employee-registration/employee-registration.component.ts
@@ -0,0 +1,30 @@
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-employee-registration',
+  templateUrl: './employee-registration.component.html',
+  styleUrls: ['./employee-registration.component.scss']
+})
+export class EmployeeRegistrationComponent implements OnInit {
+
+  departments: any = []; 
+
+  constructor(private http: HttpClient){
+
+  }
+
+  ngOnInit(): void {
+    this.loadDepartments();
+  }
+
+  loadDepartments(){
+    this.http.get("assets/departments.json").subscribe((res:any) =>{
+      //debugger;
+      this.departments = res.data;
+      console.log(res.data);
+      
+    })
+  }
+
+}
